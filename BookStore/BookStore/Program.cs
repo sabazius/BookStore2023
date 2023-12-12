e26cc72652211879ef4a945af39f14edd924ca44
Boris Serafimov <b.serafimov@draftkings.com>
2023-12-12 19:03:39 +0200

    Author repository

--- BookStore/BookStore/Program.cs
+++ BookStore/BookStore/Program.cs
@@ -1,3 +1,8 @@
+using BookStore.BL.Interfaces;
+using BookStore.BL.Services;
+using BookStore.DL.Interfaces;
+using BookStore.DL.Repositories;
+
 namespace BookStore
 {
     public class Program
@@ -7,11 +12,17 @@ namespace BookStore
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services
+                .AddSingleton<IAuthorRepository, AuthorRepository>();
+            builder.Services
+                .AddSingleton<IAuthorService, AuthorService>();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+
 
             var app = builder.Build();
 
